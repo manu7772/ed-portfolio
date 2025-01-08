6cd4c31dda7e86b146615a228ea572f5b26d2684
edujardin <manu7772@gmail.com>
2025-01-08 22:37:34 +0100

    Prepare for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://manu7772.github.io',
+  base: 'ed-portfolio',
+  trailingSlash: 'always',
   integrations: [tailwind()],
 });
